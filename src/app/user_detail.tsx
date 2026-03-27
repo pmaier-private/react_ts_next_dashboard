c0971738276edb16df06bb073070c0adfeefda4a
Paul Maier <dr.paul.maier@gmail.com>
2026-03-27 10:58:12 +0100

    TSDoc and return type

--- src/app/user_detail.tsx
+++ src/app/user_detail.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, JSX } from "react";
 import { fetchFromJsonPlaceholder, User } from "./api";
 
 type State =
@@ -8,13 +8,22 @@ type State =
     | { status: "error"; error: string }
     | { status: "success"; data: User };
 
+/**
+ * Fetch and display details for a specific user.
+ * 
+ * Using an effect the user is fetched from the jsonplaceholder API.
+ * 
+ * @param userId - The ID of the user to fetch details for.
+ * @param title - Optional title to display above the user details. 
+ * @returns Component html.
+ */
 export default function UserDetail({
     userId,
     title = "User Detail",
 }: {
     userId: number;
     title?: string;
-}) {
+}): JSX.Element {
     const [state, setState] = useState<State>({ status: "loading" });
 
     useEffect(() => {
